90e7298d41cda45e58c6ec86ef5f6f8e9f323c03
SlickJohnson <liwa.johnson@gmail.com>
2018-07-30 11:00:24 -0700

    Add Comment model

---
+++ ProductHuntExample/Comment.swift
@@ -0,0 +1,18 @@
+//
+//  Comment.swift
+//  ProductHuntExample
+//
+//  Created by Willie Johnson on 7/30/18.
+//  Copyright © 2018 Willie Johnson. All rights reserved.
+//
+
+import Foundation
+
+struct Comment: Decodable {
+  let id: Int
+  let body: String
+}
+
+struct CommentList: Decodable {
+  let comments: [Comment]?
+}
